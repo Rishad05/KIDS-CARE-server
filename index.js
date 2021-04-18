4b4b611e30ee7033f0a50512a208c9d02fc91139
Rishad05 <iu.rishad5@gmail.com>
2021-04-18 20:50:36 +0600

    editing

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(fileUpload());
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) =>{
   res.send("hello from db it's working")
 })
 
-
+app.listen(PORT)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.j70me.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -170,4 +170,4 @@ client.connect(err => {
 
 });
 
-app.listen(process.env.PORT || port)
+
